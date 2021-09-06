a6f62d61047ba68f7641d9d129b12d0a56805759
Shourav Sarker <ssarker152167@bscse.uiu.ac.bd>
2021-09-06 20:40:59 +0600

    Implemented The Singleton Pattern

--- MyFancyJavaProject/src/myfancyjavaproject/MyFancyJavaProject.java
+++ MyFancyJavaProject/src/myfancyjavaproject/MyFancyJavaProject.java
@@ -9,6 +9,39 @@ package myfancyjavaproject;
  *
  * @author Shourav
  */
+class DbConnection{
+
+    private static DbConnection instance=null;
+
+    private DbConnection(String dbUser,String dbPassword,String dbName ) { 
+    	SQLConnection connection = new SQLConnection(dbUser, dbPassword, dbName);
+   }
+
+
+	public static DbConnection getDbConnection(String dbUser,String dbPassword,String dbName) {
+      if (instance== null ) 
+		instance = new DbConnection(dbUser,dbPassword,dbName) ;                           
+
+      return instance;
+    }
+}
+
+class SQLConnection {
+	
+	String dbUser;
+	String dbPassword;
+	String dbName;
+	
+	public SQLConnection(String dbUser, String dbPassword, String dbName) {
+		super();
+		this.dbUser = dbUser;
+		this.dbPassword = dbPassword;
+		this.dbName = dbName;
+	}
+	
+
+}
+
 public class MyFancyJavaProject {
 
     /**
@@ -16,6 +49,10 @@ public class MyFancyJavaProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         String dbUser="admin";
+		 String dbPassword="admin";
+		 String dbName="tesDb";
+		 DbConnection connection=DbConnection.getDbConnection(dbUser, dbPassword, dbName);
         System.out.println("Creted A Java Project Without Singleton Pattern");
         
     }
